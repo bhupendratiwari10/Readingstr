43ffff1d9541f91f8b814bbd4ab5d314db44d968
bhupendratiwari10 <bhupendratiwari675@gmail.com>
2021-01-05 15:56:43 +0530

    Hello

---
+++ readingStr.c
@@ -0,0 +1,26 @@
+// C program to read strings
+
+#include<stdio.h>
+int main()
+    {
+// Declaring strings
+char str[1];
+
+//reading strings
+scanf("%s",str);
+
+// printing strings
+printf("%s",str);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
